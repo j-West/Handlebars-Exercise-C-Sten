6b1e4bbc043251e78c6675a93690faaaf124089e
j-west <westja89@gmail.com>
2017-01-05 10:41:27 -0600

    pretty good stuff

--- myJavascript.js
+++ myJavascript.js
@@ -1,36 +1,47 @@
 //sample data modified from randomUser.me
-var randomUser = {
-  "results": [
-    {
-      "gender": "female",
-      "name": {
-        "title": "ms",
-        "first": "stella",
-        "last": "meyer"
-      },
-      "location": {
-        "street": "7385 kapellenweg",
-        "city": "schmalkalden-meiningen",
-        "state": "saarland",
-        "postcode": 58225
-      },
-      "email": "stella.meyer@example.com",
-      "picture": {
-        "medium": "https:\/\/randomuser.me\/api\/portraits\/med\/women\/50.jpg"
-      },
-      "nat": "DE"
-    }
-  ]
-}
-
+// var randomUser = {
+//   "results": [
+//     {
+//       "gender": "female",
+//       "name": {
+//         "title": "ms",
+//         "first": "stella",
+//         "last": "meyer"
+//       },
+//       "location": {
+//         "street": "7385 kapellenweg",
+//         "city": "schmalkalden-meiningen",
+//         "state": "saarland",
+//         "postcode": 58225
+//       },
+//       "email": "stella.meyer@example.com",
+//       "picture": {
+//         "medium": "https:\/\/randomuser.me\/api\/portraits\/med\/women\/50.jpg"
+//       },
+//       "nat": "DE"
+//     }
+//   ]
+// }
+function handlebarStuff(data) {
 //handlebars step one: grap the html from the script tag
-
+var source   = $("#entryData").html();
 
 //handlebars step two: compile it into a template
+var template = Handlebars.compile(source);
 
 
 //handlebars step three:render the HTML by passing the data to the template
-
+var finished = template(data);
 
 //handlebars step four: put the complete HTML into the DOM
+$(".outputDiv").append(finished);
 
+}
+$.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+    console.log(data.results);
+    handlebarStuff(data)
+  }
+});
